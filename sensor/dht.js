bbfc913889f6da4fbeaf1b11ef1c45f2e7eb6e88
Julian Gilles <julian.gilles@googlemail.com>
2019-02-11 17:12:10 +0100

    read teamp

--- sensor/dht.js
+++ sensor/dht.js
@@ -1,5 +1,11 @@
+var sensor = require('node-dht-sensor');
+
 module.exports = {
     getValues: function () {
-        console.log('TEMP');
+	sensor.read(22, 4, function(err, temperature, humidity) {
+	    if (!err) {
+		    console.log('temp: ' + temperature.toFixed(1) + '°C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
+	    }
+	});
     }
 };
