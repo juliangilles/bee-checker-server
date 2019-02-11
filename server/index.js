9f68b3befb123b2190d71935270387907cf80455
Julian Gilles <julian.gilles@ilume.de>
2019-02-11 19:13:53 +0100

    changed the syntax

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ var express = require('express');
 var app = express();
 var dht = require('./../sensor/dht');
 
-app.get('/api/temp', async function (req, res) {
+app.get('/api/temp', async (req, res) => {
     var values = await dht.getValues();
     res.send(JSON.stringify(values));
 });
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
 });
 
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080.');
+app.listen(8080, () => {
+    console.log('bee checker is listening on port 8080.');
 });
