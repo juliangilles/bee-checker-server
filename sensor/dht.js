c2ce028630ad483ae039a483841dcd8e7b13acf9
Julian Gilles <julian.gilles@ilume.de>
2019-02-11 19:10:41 +0100

    units

--- sensor/dht.js
+++ sensor/dht.js
@@ -2,19 +2,23 @@ var sensor = require('node-dht-sensor');
 var moment = require('moment');
 
 module.exports = {
-    getValues: async function () {
+    getValues: async () => {
         return new Promise((resolve, reject) => {
-            sensor.read(22, 4, function (err, temperature, humidity) {
+            sensor.read(22, 4, (err, temperature, humidity) => {
                 if (!err) {
                     resolve({
                         temp: temperature.toFixed(1),
+                        tempUnit: '°C',
                         humidity: humidity.toFixed(1),
+                        tempUnit: '%',
                         dateTime: moment().format()
                     });
                 } else {
                     reject({
                         temp: 'N/A',
+                        tempUnit: '°C',
                         humidity: 'N/A',
+                        tempUnit: '%',
                         dateTime: moment().format()
                     })
                 }
